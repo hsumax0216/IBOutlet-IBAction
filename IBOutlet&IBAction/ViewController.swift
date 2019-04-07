054e8e679909aad50ece47ded9feacbfcb9165ba
hsumax0216 <b2626235@gmail.com>
2019-04-07 04:40:55 -0700

    第三次作業４／１６號為期限

--- IBOutlet&IBAction/ViewController.swift
+++ IBOutlet&IBAction/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UILabel!
+    @IBOutlet weak var show1: UILabel!
+    @IBOutlet weak var slider1out: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func Actionfunc(_ sender: Any) {
+        if let input=text1.text,let _number = Double(input){
+            show1.text = String(format: "%.2f", _number )
+        }
+        view.endEditing(true)
+    }
+    
+    @IBAction func slider1(_ sender: Any) {
+        text2.text = String(format: "%3.f", slider1out.value )
+    }
+    
 }
 
