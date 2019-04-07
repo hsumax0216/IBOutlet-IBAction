405c1eb5397e7876f677db8fab538a281865745c
hsumax0216 <b2626235@gmail.com>
2019-04-07 01:57:17 -0700

    Initial Commit

---
+++ IBOutlet&IBAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBOutlet&IBAction
+//
+//  Created by VMwareMojave on 2019/4/7.
+//  Copyright © 2019 VMwareMojave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
